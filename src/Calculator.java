f5910de498e520c4c1c7cd8697e6733349f803d4
Fakhreza Susilo <shadowx.susilo@yahoo.co.id>
2018-07-30 21:42:15 +0700

    [reza] minimum value

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList;
+
 public class Calculator {
 
-            
+
+
+    public Calculator() {
+
+    }
+
+    public int plus(int a , int b){
+        return a + b;
+    }
+
+    public int  kali (int a , int b){
+        return a * b;
+    }
+
+    public int minus (int a , int b){
+        return a - b;
+    }
+
+    public double bagi (int a , int b){
+        return a / b;
+    }
+
+    public int minimum(int [] list){
+        int TempValue = 50;
+        for (int i : list){
+
+
+            if ( TempValue > i ){
+                TempValue = i;
+
+            }
+
+        }
+        return TempValue;
+    }
+
 }
