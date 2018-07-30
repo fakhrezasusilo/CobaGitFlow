97f3610de0391fe84be3fe76b5755b0e3fe9f99f
bayu saputra <babahayoo@gmail.com>
2018-07-30 21:27:00 +0700

    {BAYU} Average

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,17 @@
 public class Calculator {
+	
+	public double ratarata(int[] nums){
+        
+        double dt = 0;
+        double avg = 0;
+        for(int val : nums){
+            avg += val;
+            dt++;
+        }
+        
+        return avg/dt;
+        
+    }
 
             
 }
